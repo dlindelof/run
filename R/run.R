98d1ecf9ce353d453712969f1906f409f6ab932f
David Lindelöf <dlindelof@expediagroup.com>
2019-02-07 13:48:39 +0100

    fix minor R CMD CHECK issues

--- R/run.R
+++ R/run.R
@@ -1,8 +1,15 @@
+#' Run an R package
+#'
+#' Installs an R package provided as a tarball, installs its dependencies,
+#' and runs the package's `main()` function.
+#'
+#' @param path path to a package tarball
+#' @param ... other arguments (currently ignored)
 #' @export
 run <- function(path, ...) {
   name <- package_name(path)
   remotes::install_local(path, upgrade = "never")
-  main <- getFromNamespace("main", name)
+  main <- utils::getFromNamespace("main", name)
   main()
 }
 
